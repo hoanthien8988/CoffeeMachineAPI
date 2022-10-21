e2046f82a3dc6013193a04d2f157a50f1f790ba0
david_thien_do <david.thien.do@somotsoft.com>
2022-10-21 19:52:51 +0700

    Add project files.

---
+++ CoffeeMachineAPI/CoffeeMachine.cs
@@ -0,0 +1,9 @@
+﻿namespace CoffeeMachineAPI
+{
+    public class CoffeeMachine
+    {
+        public string Message { get; set; } = string.Empty;
+        public string Prepared { get; set; } = string.Empty;
+
+    }
+}
---
+++ CoffeeMachineAPI/Controllers/CoffeeMachineController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeMachineAPI.Controllers
+{
+    [ApiController]
+    public class CoffeeMachineController : ControllerBase
+    {
+        private static int noOfCall = 1;
+
+        [HttpGet]
+        [Route("brew-coffee")]
+        public async Task<ActionResult<CoffeeMachine>> Get()
+        {
+            if (noOfCall < 5)
+            {
+                noOfCall++;
+                DateTime today = DateTime.Today;
+                if (today.Day == 1 && today.Month == 4)
+                {
+                    return this.StatusCode(StatusCodes.Status418ImATeapot, string.Empty);
+                }
+                CoffeeMachine machine = new()
+                {
+                    Message = "Your piping hot coffee is ready",
+                    Prepared = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ssK")
+                };
+                return Ok(machine);
+            }
+            else
+            {
+                return this.StatusCode(StatusCodes.Status503ServiceUnavailable, string.Empty);
+            }
+        }
+    }
+}
